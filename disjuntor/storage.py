f6c543046064e33e2941686e819eb3aa93bcf801
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-10-02 13:09:26 +0200

    Something

--- disjuntor/storage.py
+++ disjuntor/storage.py
@@ -2,13 +2,17 @@ from __future__ import annotations
 
 from collections import defaultdict
 from datetime import datetime
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import rich.repr
 
 
 class Storage:
-    def increment_success_counter(self, name: str) -> None:
+    def increment_success_counter(self, name: str) -> int:
         raise NotImplementedError()
 
-    def increment_failure_counter(self, name: str) -> None:
+    def increment_failure_counter(self, name: str) -> int:
         raise NotImplementedError()
 
     def start_timeout_timer(self, name: str) -> None:
@@ -26,15 +30,17 @@ class Storage:
 
 class MemoryStorage(Storage):
     def __init__(self) -> None:
-        self._failure_counter = defaultdict(int)
-        self._success_counter = defaultdict(int)
+        self._failure_counter: defaultdict[str, int] = defaultdict(int)
+        self._success_counter: defaultdict[str, int] = defaultdict(int)
         self._start_time: dict[str, datetime] = {}
 
-    def increment_failure_counter(self, name: str) -> None:
+    def increment_failure_counter(self, name: str) -> int:
         self._failure_counter[name] += 1
+        return self._failure_counter[name]
 
-    def increment_success_counter(self, name: str) -> None:
+    def increment_success_counter(self, name: str) -> int:
         self._success_counter[name] += 1
+        return self._success_counter[name]
 
     def start_timeout_timer(self, name: str) -> None:
         self._start_time[name] = datetime.now()
@@ -50,7 +56,7 @@ class MemoryStorage(Storage):
             return None
         return self._start_time[name]
 
-    def __rich_repr__(self):
+    def __rich_repr__(self) -> rich.repr.Result:
         yield self.__class__.__name__
         yield "failure_counter", self._failure_counter
         yield "success_counter", self._success_counter
